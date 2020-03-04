68fd022b400ca0ee412755e77c6c06bcfae63c53
StuMcCarthy <47303422+StuMcCarthy@users.noreply.github.com>
2020-03-04 22:11:54 +0000

    Added Actor Class

---
+++ MovementGame.Core/Actor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovementGame.Core
+{
+    public struct Location
+    {
+        public int X;
+        public int Y;
+        public int Z;
+    }
+    public struct Scale
+    {
+        public float X;
+        public float Y;
+        public float Z;
+    }
+    public struct Rotation
+    {
+        public float Yaw;
+        public float Pitch;
+        public float Roll;
+    }
+    public enum Visibility
+    {
+        Visible,
+        Hidden
+    }
+
+    public abstract class Actor
+    {
+        public Location Location { get; set; }
+        public Scale Scale { get; set; }
+        public Rotation Rotation { get; set; }
+        public Visibility Visibility { get; set; }
+    }
+}
