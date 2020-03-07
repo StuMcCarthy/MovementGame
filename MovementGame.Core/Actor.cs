0b13bb3d040357f45fc56712da8f58a07dc5dc0b
StuMcCarthy <47303422+StuMcCarthy@users.noreply.github.com>
2020-03-07 15:03:31 +0000

    Added Movement Actor

--- MovementGame.Core/Actor.cs
+++ MovementGame.Core/Actor.cs
@@ -3,21 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
 
 namespace MovementGame.Core
 {
-    public struct Location
-    {
-        public int X;
-        public int Y;
-        public int Z;
-    }
-    public struct Scale
-    {
-        public float X;
-        public float Y;
-        public float Z;
-    }
     public struct Rotation
     {
         public float Yaw;
@@ -32,9 +21,48 @@ namespace MovementGame.Core
 
     public abstract class Actor
     {
-        public Location Location { get; set; }
-        public Scale Scale { get; set; }
-        public Rotation Rotation { get; set; }
-        public Visibility Visibility { get; set; }
+        public Vector3D Location { get; set; }
+        public Vector3D Scale { get; private set; }
+        public Rotation Rotation { get; private set; }
+        public Visibility Visibility { get; private set; }
+
+        public Vector3D SetLocation(Vector3D newLocation)
+        {
+            Location = newLocation;
+            return Location;
+        }
+        public Vector3D SetLocation(int x, int y, int z)
+        {
+            Location = new Vector3D { X = x, Y = y, Z = z };
+            return Location;
+        }
+
+        public Vector3D SetScale(Vector3D newScale)
+        {
+            Scale = newScale;
+            return Scale;
+        }
+        public Vector3D SetScale(int x, int y, int z)
+        {
+            Scale = new Vector3D { X = x, Y = y, Z = z };
+            return Scale;
+        }
+
+        public Rotation SetRotation(Rotation newRotation)
+        {
+            Rotation = newRotation;
+            return Rotation;
+        }
+        public Rotation SetRotation(float yaw, float pitch, float roll)
+        {
+            Rotation = new Rotation { Yaw = yaw, Pitch = pitch, Roll = roll };
+            return Rotation;
+        }
+
+        public Visibility SetVisibility(Visibility newVisibility)
+        {
+            Visibility = newVisibility;
+            return Visibility;
+        }
     }
 }
---
+++ MovementGame.Core/MovementActor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Media3D;
+
+namespace MovementGame.Core
+{
+    public class MovementActor : Actor
+    {
+        public bool AffectedByGravity { get; private set; } = false;
+
+        public void SetHasGravity(bool newValue)
+        {
+            AffectedByGravity = newValue;
+        }
+
+        public void MoveActor(Vector3D movementForce)
+        {
+            Location += movementForce;
+        }
+    }
+}
